afeee4bf91e7d90ecc1ca213e4a49b89da4cc8b4
rchaser53 <tayoshizawa29@gmail.com>
2017-10-21 14:50:43 +0900

    sdl2 wasm worked in browser

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
 
-app.use('/', express.static('target/wasm32-unknown-emscripten/release/'));
+app.use('/', express.static('target/wasm32-unknown-emscripten/release/deps'));
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'))
 });
